983335b639f42231c3c853c9ad10733caffe79a9
Yun Jang <jang.yunj7@gmail.com>
2023-01-11 23:31:25 -0500

    added name, episode

--- Rick_and_Morty_API.py
+++ Rick_and_Morty_API.py
@@ -1,5 +1,17 @@
 import requests
 
-baseurl = 'https://rickandmortyapi.com/api/character'
+baseurl = 'https://rickandmortyapi.com/api/'
 
 endpoint = 'character'
+
+r = requests.get(baseurl + endpoint)
+
+data = r.json()
+
+pages = print(data['info']['pages'])
+
+name = data['results'][0]['name']
+episodes = data['results'][0]['episode']
+print(episodes)
+
+
